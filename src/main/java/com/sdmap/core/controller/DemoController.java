189637749dfa255ee6686753e2102065975ed418
dqs <dqs@dqs.com>
2021-11-25 15:51:34 +0800

    add logger example for class

--- src/main/java/com/sdmap/core/controller/DemoController.java
+++ src/main/java/com/sdmap/core/controller/DemoController.java
@@ -4,13 +4,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class DemoController {
-    
-    //基本服务示例，在命令行执行下面的curl看效果，或者直接在浏览器地址栏查看
-    //curl "http://192.168.200.137:8080/hello?who=GIS"
+
+    //private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    // 基本服务示例，在命令行执行下面的curl看效果，或者直接在浏览器地址栏查看
+    // curl "http://192.168.200.137:8080/hello?who=GIS"
     @GetMapping("/hello")
-    public String hello(@RequestParam(value="who",defaultValue = "world")String who){
+    public String hello(@RequestParam(value = "who", defaultValue = "world") String who) {
+        
+        log.debug("This is a debug message");
+        log.info("This is an info message");
+        log.warn("This is a warn message");
+        log.error("This is an error message");        
+
         return "Hello" + who;
     }
 }

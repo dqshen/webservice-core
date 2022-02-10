d1b88c2370d37cda7dbd49850ca3bcb52e723727
dqshen <dqshen@hogwarts>
2022-02-10 21:57:21 +0800

    增加使用log4j2的依赖包的说明

--- src/main/java/com/sdmap/core/controller/DemoController.java
+++ src/main/java/com/sdmap/core/controller/DemoController.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class DemoController {
 
-    //private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+    // private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     // 基本服务示例，在命令行执行下面的curl看效果，或者直接在浏览器地址栏查看
     // curl "http://192.168.200.137:8080/hello?who=GIS"
@@ -23,7 +23,26 @@ public class DemoController {
         log.info("This is an info message");
         log.warn("This is a warn message");
         log.error("This is an error message");        
+        // logsomething是一个简单类，内容如下：
+        /*
+        package com.dqs.log4jslf4jadapter;
+
+        import org.apache.logging.log4j.LogManager;
+        import org.apache.logging.log4j.Logger;
+
+        public class logsomething {
+
+            protected static final Logger Logger = LogManager.getLogger();
+
+            public static void hellolog() {
+
+                Logger.info("hello log4j2!");
+
+            }
+        } */
+
         logsomething.hellolog();
+        
 
         return "Hello" + who;
     }
